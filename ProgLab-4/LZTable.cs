70c666dd54421a2ebb1ee69a7959f37100350ff2
Dmytro <karambolrul@gmail.com>
2020-04-15 15:32:12 +0300

    Basics

---
+++ ProgLab-4/LZTable.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgLab_4
+{
+    class LZTable
+    {
+        List<string> dictionary;
+        public LZTable()
+        {
+            dictionary = new List<string>();
+        }
+        public string Add(string binaryCode)
+        {
+            string output = "";
+            for(int i = 0; i < binaryCode.Length; i++)
+            {
+                string substr = binaryCode[i].ToString();
+                for (int j = 1; j < binaryCode.Length - i + 1; j++)
+                {
+                    if (dictionary.Contains(substr) && i + j != binaryCode.Length)
+                    {
+                        substr += binaryCode[i + j].ToString();
+                    }
+                    else
+                    {
+                        if(i + j != binaryCode.Length)
+                        {
+                            dictionary.Add(substr);
+                            i += j - 2;
+                        }
+                        else
+                        {
+                            dictionary.Add(substr);
+                            output += dictionary.IndexOf(substr) + " ";
+                            break;
+                        }
+
+                        if (substr.Length != 1)
+                            output += dictionary.IndexOf(substr.Remove(substr.Length - 1)) + " ";
+                        break;
+                    }
+                }
+            }
+            return output;
+        }
+    }
+}
---
+++ ProgLab-4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ProgLab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LZTable table = new LZTable();
+            Console.WriteLine(table.Add("fu fu fuck fucking"));
+            Console.ReadKey();
+        }
+    }
+}
